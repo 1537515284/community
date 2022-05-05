c264a5a95949a5f59fc5be675c94a1ca94896cba
ls <1537515284@qq.com>
2022-05-05 14:59:48 +0800

    add publish

---
+++ src/main/java/com/ls/community/mapper/QuestionMapper.java
@@ -0,0 +1,23 @@
+package com.ls.community.mapper;
+
+import com.ls.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author LS
+* @description 针对表【QUESTION】的数据库操作Mapper
+* @createDate 2022-05-04 21:40:35
+* @Entity com.ls.community.model.Question
+*/
+@Mapper
+public interface QuestionMapper {
+
+    @Insert("INSERT INTO question(title,description,gmt_create,gmt_modified,creator,tag) VALUES(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create(Question question);
+
+}
+
+
+
+
